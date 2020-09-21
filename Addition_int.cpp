75e89d89edb01f20b726373f44f674d53019ffca
Sahil Shama <shamasahil240@gmail.com>
2020-09-22 00:00:29 +0530

    Addition of two int datatypes

---
+++ Addition_int.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1;
+  int num2;
+  int sum;
+
+  cout << "Please enter the first integer \n";
+  cin >> num1;
+  cout << "Please enter the second integer \n";
+  cin >> num2;
+  
+  sum = num1+num2;
+  cout << "Sum of both integers is " << sum <<"\n";
+
+  return 0;
+}
